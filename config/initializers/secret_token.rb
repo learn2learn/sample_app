9b94082dc682eefd2f5a282012bbd88330685d79
Nirmal Merchant <nirmal_merchant@mckinsey.com>
2011-06-24 11:51:56 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6c0b9c2bc0f7bfbaf10416f7897fc115b139961563555b4da70262995f3098a9cbe43804e7822b8a10e023cf627593a0cb65396b78609a7e8bba16252e069e9f'
